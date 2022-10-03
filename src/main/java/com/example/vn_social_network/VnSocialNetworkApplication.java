2ea138f5573f57abab78d0e05bb3a3b629d36007
= <=>
2022-10-03 14:41:37 +0700

    update

--- src/main/java/com/example/vn_social_network/VnSocialNetworkApplication.java
+++ src/main/java/com/example/vn_social_network/VnSocialNetworkApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VnSocialNetworkApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(VnSocialNetworkApplication.class, args);
     }
