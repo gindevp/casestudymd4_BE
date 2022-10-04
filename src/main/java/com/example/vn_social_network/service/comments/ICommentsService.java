15df3157258dc12a8a66694112f745263dcd8204
LENOVO <imbakito1604@gmail.com>
2022-10-04 09:34:58 +0700

    comments

---
+++ src/main/java/com/example/vn_social_network/service/comments/ICommentsService.java
@@ -0,0 +1,9 @@
+package com.example.vn_social_network.service.comments;
+
+import com.example.vn_social_network.model.action.Comments;
+import com.example.vn_social_network.service.IGeneralService;
+
+
+
+public interface ICommentsService extends IGeneralService<Comments> {
+}
