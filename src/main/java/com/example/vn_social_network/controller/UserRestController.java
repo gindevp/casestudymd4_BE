fa370b31016f4139155df9624462ee740349d354
= <=>
2022-10-04 08:30:57 +0700

    upadte async

--- src/main/java/com/example/vn_social_network/controller/UserRestController.java
+++
@@ -1,9 +0,0 @@
-package com.example.vn_social_network.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/VnSN")
-public class UserRestController {
-}
--- src/main/java/com/example/vn_social_network/repository/UserRepository.java
+++ src/main/java/com/example/vn_social_network/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.example.vn_social_network.repository;
 
-import com.example.vn_social_network.model.users.Users;
+import com.example.vn_social_network.model.app_users.AppUsers;
 import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<Users, Long> {
+public interface UserRepository extends JpaRepository<AppUsers, Long> {
 }
