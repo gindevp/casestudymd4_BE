272b06367d8bb58d082510920ea0d91564d7bb6d
LENOVO <imbakito1604@gmail.com>
2022-10-05 09:00:52 +0700

    them file likes

---
+++ src/main/java/com/example/vn_social_network/service/action/like/LikesService.java
@@ -0,0 +1,35 @@
+package com.example.vn_social_network.service.action.like;
+
+import com.example.vn_social_network.model.action.Likes;
+import com.example.vn_social_network.repository.ILikesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class LikesService implements ILikesService {
+
+    @Autowired
+    ILikesRepository likesRepository;
+    @Override
+    public Iterable findAll() {
+        return likesRepository.findAll();
+    }
+
+    @Override
+    public Optional findById(Long id) {
+        return likesRepository.findById(id);
+    }
+
+    @Override
+    public Likes save(Likes likes) {
+        return likesRepository.save(likes);
+    }
+
+    @Override
+    public void remove(Long id) {
+        likesRepository.findById(id);
+    }
+}
